26b4346fb8240e571b462e4e7f4f51d98f57dccc
bestw <devspeed.vision.it@gmail.com>
2021-12-02 18:54:14 +0700

    app.js update

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            screen.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
